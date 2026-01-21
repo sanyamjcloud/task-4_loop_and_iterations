a2597042c4fcbdf81f3fdd520e4d3109641fad30
Sanyam Jain <sanyamj.cloud@gmail.com>
2026-01-21 19:00:26 +0530

    Update task4_break_continue.py

--- task4_break_continue.py
+++ task4_break_continue.py
@@ -1,1 +1,10 @@
-
+print("Task 4: Break and Continue example")
+for i in range(1, 11):
+    if i == 5:
+        print("Skipping number 5")
+        continue  # Skip number 5
+    if i == 8:
+        print("Stopping at number 8")
+        break  # Stop loop at 8
+    print(i)
+print()
