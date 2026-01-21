8ae073af36cf20e003c6c045bf0e28263c6b5a15
Sanyam Jain <sanyamj.cloud@gmail.com>
2026-01-21 19:02:49 +0530

    Update task9_real_world_example.py

--- task9_real_world_example.py
+++ task9_real_world_example.py
@@ -1,1 +1,9 @@
+print("Task 9: Real-world example - shopping cart")
+shopping_cart = ["apple", "banana", "chocolate", "milk"]
+for item in shopping_cart:
+    if item == "chocolate":
+        print(f"{item} is on discount!")
+    else:
+        print(f"Adding {item} to the bag")
+print()
 
